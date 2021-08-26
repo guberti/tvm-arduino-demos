49d8638bdd3e524c05b7a50afffdee95215c356c
Gavin Uberti <gavin.uberti@gmail.com>
2021-08-26 14:13:26 -0700

    Switch generate_project to use AOT

--- generate_project.py
+++ generate_project.py
@@ -6,12 +6,8 @@ import tflite
 import tvm
 from tvm import relay, micro
 
-TARGET = "c -keys=cpu -link-params=1 -mcpu=cortex-m33 -model=nrf5340dk -runtime=c -system-lib=1"
-
 def generate_project(input_model, output_dir):
-    # Make sure our output directory exists
     output_dir = Path(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
 
     # The directory inside TVM where templates are stored
     template_dir = Path(tvm.__file__).parents[2] / "apps" / "microtvm" / "arduino" / "template_project"
@@ -21,14 +17,19 @@ def generate_project(input_model, output_dir):
         tflite_model_buf = f.read()
     tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
     mod, params = relay.frontend.from_tflite(tflite_model)
+
+    # Code will run on ALL Arduinos, but will be optimized for the cxd5602gg processor
+    target = tvm.target.target.micro(
+        "cxd5602gg", options=["--link-params=1", "--unpacked-api=1", "--executor=aot"]
+    )
     with tvm.transform.PassContext(opt_level=3, config={"tir.disable_vectorize": True}):
-        mod = relay.build(mod, TARGET, params=params)
+        mod = relay.build(mod, target, params=params)
 
     project = tvm.micro.generate_project(
         str(template_dir.resolve()),
         mod,
         str(output_dir.resolve()),
-        {"arduino_board": "spresense", "arduino_cmd": "arduino-cli", "verbose": 0},
+        {"arduino_board": "spresense", "arduino_cmd": "arduino-cli", "verbose": 0, "project_type": "example_project"},
     )
 
 
